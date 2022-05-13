d9389b2d6d11f207271bdfca3bf53f7f173982ad
Werick <werick2000@live.com>
2022-05-12 22:05:33 -0300

    Desafio de codigo

---
+++ DioDesafioDeCodigo/Enum/Genero.cs
@@ -0,0 +1,22 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Acao = 1,
+		Aventura = 2,
+		Comedia = 3,
+		Classicos = 4,
+		Documentario = 5,
+		Drama = 6,
+		Espionagem = 7,
+		Estrangeiros = 8,
+		Faroeste = 9,
+		Fantasia = 10,
+		Ficcao_Cientifica = 11,
+		Musical = 12,
+		Romance = 13,
+		Suspense = 14,
+		Terror = 15,
+		
+	}
+}
